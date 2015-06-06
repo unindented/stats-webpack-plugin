2e7195ff900613b16b76ece747127b386ffd973e
James Ide <ide@jameside.com>
2015-06-05 18:38:29 -0700

    Create the directory for the stats file if it doesn't exist
    If the directory to your stats file doesn't yet exist, this plugin throws an error. Think it's nicer to create the directiry if needed before writing the stats file.

--- test/plugin.js
+++ test/plugin.js
@@ -1,5 +1,7 @@
+var clone = require('lodash-node/modern/lang/clone');
 var fs = require('fs');
 var path = require('path');
+var rimraf = require('rimraf');
 var webpack = require('webpack');
 var StatsPlugin = require('../');
 
@@ -7,13 +9,15 @@ var inputFolder = path.join(__dirname, 'fixtures');
 var inputFile = path.join(inputFolder, 'entry.js');
 var outputFolder = path.join(__dirname, 'output');
 var outputFile = path.join(outputFolder, 'stats.json');
+var newOutputFolder = path.join(__dirname, 'new-output');
+var newOutputFile = path.join(newOutputFolder, 'stats.json');
 
 var options = {
   chunkModules: true,
   exclude: [/node_modules[\\\/]/]
 };
 
-var compiler = webpack({
+var defaultCompilerOptions = {
   entry: inputFile,
 
   output: {
@@ -24,17 +28,36 @@ var compiler = webpack({
   plugins: [
     new StatsPlugin(outputFile, options)
   ]
-});
+}
 
 module.exports.test = {
 
   'generates `stats.json` file': function (test) {
+    var compiler = webpack(defaultCompilerOptions);
     compiler.run(function (err, stats) {
       var expected = JSON.stringify(stats.toJson(options));
       var actual = fs.readFileSync(outputFile);
       test.equal(actual, expected);
       test.done();
     });
-  }
+  },
 
+  'creates directories if they do not exist': function (test) {
+    // Ensure the output folder does not exist
+    rimraf.sync(newOutputFolder);
+
+    var compilerOptions = clone(defaultCompilerOptions, /* isDeep */ true);
+    compilerOptions.output.path = newOutputFolder;
+    compilerOptions.plugins = [
+      new StatsPlugin(newOutputFile, options)
+    ];
+
+    var compiler = webpack(compilerOptions);
+    compiler.run(function (err, stats) {
+      var expected = JSON.stringify(stats.toJson(options));
+      var actual = fs.readFileSync(newOutputFile);
+      test.equal(actual, expected);
+      test.done();
+    });
+  }
 };
